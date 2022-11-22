9f2dde6410b1549950f77a6d86aadc755a74197c
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2022-11-22 10:21:34 +0530

    File Structure

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,22 @@
+package LinkedList;
+class LL{
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+}
+public class LinkedList {
+    public static void main(String[] args) {
+
+    }
+}
