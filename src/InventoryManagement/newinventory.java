b19238c04b56461a0d5f7b62bc96821143c47c8f
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2022-12-13 14:39:55 +0530

    Inventory managmetmt

--- src/InventoryManagement/newinventory.java
+++ src/InventoryManagement/newinventory.java
@@ -16,16 +16,19 @@ class Inventory {
     }
 
     public List<Product> getProducts() {
-
         return this.products;
     }
     public void showProductsDetails(){
-        System.out.println("Product Name   "+"   ID"+"   Quantity"+"   price"+"   cost");
+        System.out.println("======================================================================================");
+        System.out.println("Product Name   "+"   ID"+"      Quantity"+"   price"+"   cost");
         for (int i=0 ; i<products.size();i++){
 
-            System.out.println(products.get(i).getName()+"   "+products.get(i).getId()+"   "+products.get(i).getQuantity()+"   "+products.get(i).getPrice()+"   "+products.get(i).getCost());
+            System.out.println(products.get(i).getName()+"  ||  "+products.get(i).getId()+"  ||   "+products.get(i).getQuantity()+"   ||  "+products.get(i).getPrice()+"  ||  "+products.get(i).getCost());
         }
+        System.out.println("======================================================================================");
+
     }
+
 
     public Product getProduct(String productId) {
         for (Product product : products) {
@@ -36,10 +39,10 @@ class Inventory {
         return null;
     }
 
-    public void purchaseProduct(String productId, int quantity) {
+    public void sellProduct(String productId, int quantity) {
         Product product = getProduct(productId);
         if (product != null) {
-            product.purchase(quantity);
+            product.selling(quantity);
         }
     }
 
@@ -65,6 +68,7 @@ class Inventory {
     private int quantity;
     private int price;
     private int cost;
+    private int NoOfItemsSold;
 
     public Product(String id, String name, int quantity, int price, int cost) {
         this.id = id;
@@ -72,6 +76,7 @@ class Inventory {
         this.quantity = quantity;
         this.price = price;
         this.cost = cost;
+        this.NoOfItemsSold =0;
     }
 
     public String getId() {
@@ -94,18 +99,26 @@ class Inventory {
         return this.cost;
     }
 
-    public void purchase(int quantity) {
-        this.quantity -= quantity;
-        System.out.println("You Purchase the product:"+ name);
+    public void selling(int quantity) {
+        System.out.println("======================================================================================");
+        System.out.println("You Sold the product: "+ name + " Need to ship the Item");
+        System.out.println("======================================================================================");
     }
 
     public void ship(int quantity) {
         this.quantity -= quantity;
+        this.NoOfItemsSold += 1;
+        System.out.println("======================================================================================");
         System.out.println("Product "+ name + "Shipped" );
+        System.out.println("======================================================================================");
     }
 
     public int calculateProfit() {
-        return (this.price - this.cost) * this.quantity;
+        if (quantity == 10){
+            return 0;
+        }
+
+        return (this.price - this.cost) * this.NoOfItemsSold;
     }
 
 }
@@ -117,112 +130,137 @@ public class newinventory {
 
         // Create a new inventory object
         Inventory inventory = new Inventory();
-        Product p1 = new Product("laptop", "lenovo-slim", 5,50000,45000);
-        Product p2 = new Product("SSD", "Samsung-980", 7,7000,4500);
-        Product p3 = new Product("Keyboard", "HP-69", 2,1200,1000);
-        Product p4 = new Product("Monitor", "Apple-M2", 6,70000,45000);
+        Product p1 = new Product("laptop", "lenovo-slim", 10, 50000, 45000);
+        Product p2 = new Product("SSD", "Samsung-980", 10, 7000, 4500);
+        Product p3 = new Product("Keyboard", "HP-69", 10, 1200, 1000);
+        Product p4 = new Product("Monitor", "Apple-M2", 10, 70000, 45000);
+        Product p5 = new Product("Monitor", "Apple-M2", 10, 70000, 45000);
         inventory.addProduct(p1);
         inventory.addProduct(p2);
         inventory.addProduct(p3);
         inventory.addProduct(p4);
-        System.out.println(inventory.getProduct("laptop"));
+        System.out.println(inventory.getProduct("laptop").getName());
         inventory.showProductsDetails();
 
-
-                    System.out.print("Enter product ID: ");
-                    String id = in.next();
-                    Product product = inventory.getProduct(id);
-                    if (product == null) {
-                        System.out.println("Invalid product ID!");
-                    } else {
-                        System.out.print("Enter quantity: ");
-                        int quantity = in.nextInt();
-                        inventory.purchaseProduct(id, quantity);
-                    }
-
-        inventory.showProductsDetails();
-
-
-//         *********Shipping
-                    System.out.print("Enter product ID: ");
-                    String id2 = in.next();
-                    product = inventory.getProduct(id2);
-                    if (product == null) {
-                        System.out.println("Invalid product ID!");
-                    } else {
-                        System.out.print("Enter quantity: ");
-                        int quantity = in.nextInt();
-                        inventory.shipProduct(id2, quantity);
-                    }
-
-
-
-
-
-//        while (true) {
-//            // Print the main menu
-//            System.out.println("Inventory Management System");
-//            System.out.println("1. List of all products");
-//            System.out.println("2. Display individual product information");
-//            System.out.println("3. Purchase");
-//            System.out.println("4. Shipping");
-//            System.out.println("5. Balance stock");
-//            System.out.println("6. Loss and Profit calculation");
-//            System.out.println("0. Exit");
-//            System.out.print("Enter your choice: ");
 //
-//            // Read the user's choice
-//            int choice = in.nextInt();
-//
-//            // Perform the selected operation
-//            switch (choice) {
-//                case 1:
-//                    // List all products
-//                    inventory.listProducts();
-//                    break;
-//                case 2:
-//                    // Display individual product information
 //                    System.out.print("Enter product ID: ");
 //                    String id = in.next();
 //                    Product product = inventory.getProduct(id);
 //                    if (product == null) {
 //                        System.out.println("Invalid product ID!");
 //                    } else {
-//                        product.displayInformation();
+//                        System.out.print("Enter quantity: ");
+//                        int quantity = in.nextInt();
+//                        inventory.purchaseProduct(id, quantity);
 //                    }
-//                    break;
-//                case 3:
-//                    // Purchase
+//
+//        inventory.showProductsDetails();
+//        System.out.println("******************************************************************************************************************");
+//
+////         *********Shipping
 //                    System.out.print("Enter product ID: ");
-//                    id = in.next();
-//                    product = inventory.getProduct(id);
+//                    String id2 = in.next();
+//                    product = inventory.getProduct(id2);
 //                    if (product == null) {
 //                        System.out.println("Invalid product ID!");
 //                    } else {
 //                        System.out.print("Enter quantity: ");
 //                        int quantity = in.nextInt();
-//                        inventory.purchase(product, quantity);
+//                        inventory.shipProduct(id2, quantity);
 //                    }
-//                    break;
-//                case 4:
-//                    // Shipping
-//                    System.out.print("Enter product ID: ");
-//                    id = in.next();
-//                    product = inventory.getProduct(id);
-//                    if (product == null) {
-//                        System.out.println("Invalid product ID!");
-//                    } else {
-//                        System.out.print("Enter quantity: ");
-//                        int quantity = in.nextInt();
-//                        inventory.ship(product, quantity);
-//                    }
-//                    break;
-//                case 5:
-//                    // Balance stock
-//                    inventory.displayStockInformation();
-//                    break;
-//                case 6:
-//                    // Loss and Profit calculation
-//                    inventory.calculateLossAndProfit
+//
+//        inventory.showProductsDetails();
+//        System.out.println("******************************************************************************************************************");
+//        // ************Balance stock
+////                    inventory.displayStockInformation();
+//
+//        // *************Loss and Profit calculation
+//
+//                    inventory.calculateProfit();
+//        inventory.showProductsDetails();
+//        System.out.println("******************************************************************************************************************");
+//
+
+
+        while (true) {
+            // Print the main menu
+            System.out.println("Inventory Management System");
+            System.out.println("1. List of all products");
+            System.out.println("2. Display individual product information");
+            System.out.println("3. Sell");
+            System.out.println("4. Shipping");
+            System.out.println("5. Balance stock");
+            System.out.println("6. Loss and Profit calculation");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+
+            // Read the user's choice
+            int choice = in.nextInt();
+
+            // Perform the selected operation
+            switch (choice) {
+                case 1:
+                    // List all products
+                    inventory.getProducts();
+                    break;
+                case 2:
+                    // Display individual product information
+                    System.out.print("Enter product ID: ");
+                    String id = in.next();
+                    Product product = inventory.getProduct(id);
+                    if (product == null) {
+                        System.out.println("Invalid product ID!");
+                    } else {
+                        System.out.println("******************************************************************************************************************");
+                        System.out.println("Name:: "+inventory.getProduct(id).getName());
+                        System.out.println("ID:: "+inventory.getProduct(id).getId());
+                        System.out.println("InStock:: "+inventory.getProduct(id).getQuantity());
+                        System.out.println("Price:: "+inventory.getProduct(id).getPrice());
+                        System.out.println("Cost:: "+inventory.getProduct(id).getCost());
+                        System.out.println("******************************************************************************************************************");
+
+                    }
+                    break;
+
+                case 3:
+                    // Purchase
+                    System.out.print("Enter product ID: ");
+                    String id2 = in.next();
+                    Product product2 = inventory.getProduct(id2);
+                    if (product2 == null) {
+                        System.out.println("Invalid product ID!");
+                    } else {
+                        System.out.print("Enter quantity: ");
+                        int quantity = in.nextInt();
+                        inventory.sellProduct(id2, quantity);
+                    }
+                    break;
+                case 4:
+                    // Shipping
+                    System.out.print("Enter product ID: ");
+                    id = in.next();
+                    product = inventory.getProduct(id);
+                    if (product == null) {
+                        System.out.println("Invalid product ID!");
+                    } else {
+                        System.out.print("Enter quantity: ");
+                        int quantity = in.nextInt();
+                        inventory.shipProduct(id, quantity);
+                    }
+                    break;
+                case 5:
+                    // Balance stock
+                    inventory.showProductsDetails();
+                    break;
+                case 6:
+                    // Loss and Profit calculation
+                    inventory.calculateProfit();
+                    break;
+                default:
+                    System.out.println("Invalid Input");
+
+            }
+
+        }
     }
 }
