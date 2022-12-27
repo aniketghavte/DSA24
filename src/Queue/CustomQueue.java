aabcf22100385108cff4801258350e196d48304a
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2022-12-27 23:19:49 +0530

    queue

--- src/Queue/CustomQueue.java
+++ src/Queue/CustomQueue.java
@@ -1,5 +1,28 @@
 package Queue;
 
+class custom_queue{
+    private int[] data;
+    private static final int DEFAULT__SIZE = 10;
+
+    int end = 0;
+
+    public custom_queue(int size) {
+        this.data = new int[size];
+    }
+
+    public custom_queue() {
+        this(DEFAULT__SIZE);
+    }
+
+
+    private boolean isFull() {
+        return end == data.length ;
+    }
+    private boolean isEmpty() {
+        return end == 0;
+    }
+}
+
 public class CustomQueue {
     public static void main(String[] args) {
 
