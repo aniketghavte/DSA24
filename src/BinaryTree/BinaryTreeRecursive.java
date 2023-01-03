913ab3d174190d91c16cc3e2f156cb2cf7a9eefa
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2023-01-03 19:37:13 +0530

    Inorder and Preorder Travarsal

--- src/BinaryTree/BinaryTreeRecursive.java
+++ src/BinaryTree/BinaryTreeRecursive.java
@@ -25,10 +25,32 @@ public class BinaryTreeRecursive {
             return newNode;
         }
     }
+
+    public static void preOrder(Node root){
+
+        if (root == null){
+            System.out.print("Null ");
+            return;
+        }
+        System.out.print(root.data+ " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void inOrder(Node root){
+        if (root == null){
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+        System.out.println("PreOrder Traversal");
+        inOrder(root);
     }
 }
