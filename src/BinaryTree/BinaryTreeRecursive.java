4bd4e2aa92a11500f02a63111258beb1505d6d78
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2023-01-04 22:53:10 +0530

    LevelOrder

--- src/BinaryTree/BinaryTreeRecursive.java
+++ src/BinaryTree/BinaryTreeRecursive.java
@@ -1,5 +1,9 @@
 package BinaryTree;
 
+import javax.swing.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreeRecursive {
     static class Node{
         int data;
@@ -45,12 +49,53 @@ public class BinaryTreeRecursive {
         System.out.print(root.data+" ");
         inOrder(root.right);
     }
+
+    public static void postOrder(Node root){
+        if (root == null){
+            return;
+        }
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+ " ");
+    }
+    public static void levelOrder(Node root){
+        if (root == null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()){
+            Node currNode = q.remove();
+            if (currNode == null){
+                System.out.println();
+                if (q.isEmpty()){
+                    break;
+                }else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data+ " ");
+                if (currNode.left != null){
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
         System.out.println("PreOrder Traversal");
-        inOrder(root);
+        postOrder(root);
+        System.out.println();
+        levelOrder(root);
     }
 }
