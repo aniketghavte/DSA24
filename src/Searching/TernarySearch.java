32f908d9fdbd0f72fcc553e78d53358356f7f56a
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2023-02-03 23:41:02 +0530

    Iterative BST

--- src/Searching/TernarySearch.java
+++ src/Searching/TernarySearch.java
@@ -1,89 +1,75 @@
-package Searching;
+// Java program to implement iterative preorder traversal
+import java.util.Stack;
 
-// Java program to illustrate
-// recursive approach to ternary search
+// A binary tree node
+class Node {
 
-class GFG {
+    int data;
+    Node left, right;
 
-    // Function to perform Ternary Search
-    static int ternarySearch(int l, int r, int key, int ar[])
+    Node(int item)
     {
-        if (r >= l) {
-
-            // Find the mid1 and mid2
-            int mid1 = l + (r - l) / 3;
-            int mid2 = r - (r - l) / 3;
-
-            // Check if key is present at any mid
-            if (ar[mid1] == key) {
-                return mid1;
-            }
-            if (ar[mid2] == key) {
-                return mid2;
-            }
-
-            // Since key is not present at mid,
-            // check in which region it is present
-            // then repeat the Search operation
-            // in that region
-
-            if (key < ar[mid1]) {
-
-                // The key lies in between l and mid1
-                return ternarySearch(l, mid1 - 1, key, ar);
-            }
-            else if (key > ar[mid2]) {
-
-                // The key lies in between mid2 and r
-                return ternarySearch(mid2 + 1, r, key, ar);
-            }
-            else {
-
-                // The key lies in between mid1 and mid2
-                return ternarySearch(mid1 + 1, mid2 - 1, key, ar);
-            }
-        }
-
-        // Key not found
-        return -1;
-    }
-
-    // Driver code
-    public static void main(String args[])
-    {
-        int l, r, p, key;
-
-        // Get the array
-        // Sort the array if not sorted
-        int ar[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-        // Starting index
-        l = 0;
-
-        // length of array
-        r = 9;
-
-        // Checking for 5
-
-        // Key to be searched in the array
-        key = 5;
-
-        // Search the key using ternarySearch
-        p = ternarySearch(l, r, key, ar);
-
-        // Print the result
-        System.out.println("Index of " + key + " is " + p);
-
-        // Checking for 50
-
-        // Key to be searched in the array
-        key = 50;
-
-        // Search the key using ternarySearch
-        p = ternarySearch(l, r, key, ar);
-
-        // Print the result
-        System.out.println("Index of " + key + " is " + p);
+        data = item;
+        left = right = null;
     }
 }
 
+class BinaryTree {
+
+    Node root;
+
+    void iterativePreorder()
+    {
+        iterativePreorder(root);
+    }
+
+    // An iterative process to print preorder traversal of Binary tree
+    void iterativePreorder(Node node)
+    {
+
+        // Base Case
+        if (node == null) {
+            return;
+        }
+
+        // Create an empty stack and push root to it
+        Stack<Node> nodeStack = new Stack<Node>();
+        nodeStack.push(root);
+
+		/* Pop all items one by one. Do following for every popped item
+		a) print it
+		b) push its right child
+		c) push its left child
+		Note that right child is pushed first so that left is processed first */
+        while (nodeStack.empty() == false) {
+
+            // Pop the top item from stack and print it
+            Node mynode = nodeStack.peek();
+            System.out.print(mynode.data + " ");
+            nodeStack.pop();
+
+            // Push right and left children of the popped node to stack
+            if (mynode.right != null) {
+                nodeStack.push(mynode.right);
+            }
+            if (mynode.left != null) {
+                nodeStack.push(mynode.left);
+            }
+        }
+    }
+
+    // driver program to test above functions
+    public static void main(String args[])
+    {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(10);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(2);
+        tree.root.left.left = new Node(3);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(2);
+        tree.iterativePreorder();
+    }
+}
+
+// This code has been contributed by Mayank Jaiswal
