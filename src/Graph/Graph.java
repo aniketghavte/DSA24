196e05cd6b4512f9d22eb2e3b0cc7847daeb9417
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2023-02-05 23:24:14 +0530

    Graph

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,18 +1,21 @@
-package Graph;
-
-// Java program to print BFS traversal from a given source
-// vertex. BFS(int s) traverses vertices reachable from s.
+// Java program to print DFS
+// traversal from a given
+// graph
 import java.io.*;
 import java.util.*;
 
-// This class represents a directed graph using adjacency
+// This class represents a
+// directed graph using adjacency
 // list representation
 class Graph {
     private int V; // No. of vertices
-    private LinkedList<Integer> adj[]; // Adjacency Lists
+
+    // Array of lists for
+    // Adjacency List Representation
+    private LinkedList<Integer> adj[];
 
     // Constructor
-    Graph(int v)
+    @SuppressWarnings("unchecked") Graph(int v)
     {
         V = v;
         adj = new LinkedList[v];
@@ -21,43 +24,45 @@ class Graph {
     }
 
     // Function to add an edge into the graph
-    void addEdge(int v, int w) { adj[v].add(w); }
-
-    // prints BFS traversal from a given source s
-    void BFS(int s)
+    void addEdge(int v, int w)
     {
-        // Mark all the vertices as not visited(By default
-        // set as false)
-        boolean visited[] = new boolean[V];
+        adj[v].add(w); // Add w to v's list.
+    }
 
-        // Create a queue for BFS
-        LinkedList<Integer> queue
-                = new LinkedList<Integer>();
+    // A function used by DFS
+    void DFSUtil(int v, boolean visited[])
+    {
+        // Mark the current node as visited and print it
+        visited[v] = true;
+        System.out.print(v + " ");
 
-        // Mark the current node as visited and enqueue it
-        visited[s] = true;
-        queue.add(s);
-
-        while (queue.size() != 0) {
-            // Dequeue a vertex from queue and print it
-            s = queue.poll();
-            System.out.print(s + " ");
-
-            // Get all adjacent vertices of the dequeued
-            // vertex s If a adjacent has not been visited,
-            // then mark it visited and enqueue it
-            Iterator<Integer> i = adj[s].listIterator();
-            while (i.hasNext()) {
-                int n = i.next();
-                if (!visited[n]) {
-                    visited[n] = true;
-                    queue.add(n);
-                }
-            }
+        // Recur for all the vertices adjacent to this
+        // vertex
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
         }
     }
 
-    // Driver method to
+    // The function to do DFS traversal.
+    // It uses recursive
+    // DFSUtil()
+    void DFS(int v)
+    {
+        // Mark all the vertices as
+        // not visited(set as
+        // false by default in java)
+        boolean visited[] = new boolean[V];
+
+        // Call the recursive helper
+        // function to print DFS
+        // traversal
+        DFSUtil(v, visited);
+    }
+
+    // Driver's Code
     public static void main(String args[])
     {
         Graph g = new Graph(4);
@@ -70,11 +75,11 @@ class Graph {
         g.addEdge(3, 3);
 
         System.out.println(
-                "Following is Breadth First Traversal "
+                "Following is Depth First Traversal "
                         + "(starting from vertex 2)");
 
-        g.BFS(2);
+        // Function call
+        g.DFS(2);
     }
 }
 // This code is contributed by Aakash Hasija
-
